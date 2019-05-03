cd6da9e7a96e2c3730a8a9e754c1da1cfd8ee1d2
Em <julietkiboi@gmail.com>
2019-05-03 15:46:56 +0300

    listing books

--- books.php
+++ books.php
@@ -14,44 +14,45 @@ $id = $title = $author = $publisher = $year_of_publication = $isbn_number = $cat
 
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['create_new'])) { // creating new
-        if (
-
-            empty(trim($_POST["title"])) &&
-            empty(trim($_POST["author"])) &&
-            empty(trim($_POST["publisher"])) &&
-            empty(trim($_POST["year_of_publication"])) &&
-            empty(trim($_POST["isbn_number"])) &&
-            empty(trim($_POST["category"])) &&
-            empty(trim($_POST["description"])) &&
-            empty(trim($_POST["user_id"])) &&
-            empty(trim($_POST["photo"]))
-
-        ) {
-
-            $form_error = "Please fill the form correctly.";
-
-        } else {
-            $title = $_POST["title"];
-            $author = $_POST["author"];
-            $publisher = $_POST["publisher"];
-            $year_of_publication = $_POST["year_of_publication"];
-            $isbn_number = $_POST["isbn_number"];
-            $category = $_POST["category"];
-            $description = $_POST["description"];
-            $user_id = $_SESSION['id'];
-            $photo = '/bookimage/' . $_POST["photo"];
-            $created_at = new DateTime();
-            $updated_at = new DateTime();
-
-
-            $sql = "INSERT into books(title, author, publisher, year_of_publication, isbn_number, category, description, user_id, photo) VALUES ('$title', '$author', '$publisher', '$year_of_publication', '$isbn_number', '$category', '$description', '$user_id', '$photo')";
-
-            $enter = mysqli_query($con, $sql);
-
-        }
-    } elseif (isset($_POST['create_edit'])) {
-        $book_id = $_POST["book_id"];
+//    if (isset($_POST['create_new'])) { // creating new
+//        if (
+//
+//            empty(trim($_POST["title"])) &&
+//            empty(trim($_POST["author"])) &&
+//            empty(trim($_POST["publisher"])) &&
+//            empty(trim($_POST["year_of_publication"])) &&
+//            empty(trim($_POST["isbn_number"])) &&
+//            empty(trim($_POST["category"])) &&
+//            empty(trim($_POST["description"])) &&
+//            empty(trim($_POST["user_id"])) &&
+//            empty(trim($_POST["photo"]))
+//
+//        ) {
+//
+//            $form_error = "Please fill the form correctly.";
+//
+//        } else {
+//            $title = $_POST["title"];
+//            $author = $_POST["author"];
+//            $publisher = $_POST["publisher"];
+//            $year_of_publication = $_POST["year_of_publication"];
+//            $isbn_number = $_POST["isbn_number"];
+//            $category = $_POST["category"];
+//            $description = $_POST["description"];
+//            $user_id = $_SESSION['id'];
+//            $photo = '/bookimage/' . $_POST["photo"];
+//            $created_at = new DateTime();
+//            $updated_at = new DateTime();
+//
+//
+//            $sql = "INSERT into books(title, author, publisher, year_of_publication, isbn_number, category, description, user_id, photo) VALUES ('$title', '$author', '$publisher', '$year_of_publication', '$isbn_number', '$category', '$description', '$user_id', '$photo')";
+//
+//            $enter = mysqli_query($con, $sql);
+//
+//        }
+//    }
+    if (isset($_POST['create_edit'])) {
+        $bookId = $_POST["book_id"];
 
         // echo $books_id;
 
@@ -71,11 +72,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         $edit = mysqli_query($con, $sql);
 
-    } elseif (isset($_POST["delete_action"])) {
+    }
+    elseif (isset($_POST["delete_action"])) {
 
-        $book_id = $_POST["book_id"];
+        $bookId = $_POST["book_id"];
 
-        $sql = "DELETE from books WHERE id='$book_id'";
+        $sql = "DELETE from books WHERE id='$bookId'";
 
         if (mysqli_query($con, $sql)) {
             header("location: books.php");
@@ -85,24 +87,26 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     } elseif (isset($_POST['edit_action'])) {
 
-        $book_id = $_POST["book_id"];
+        header('Location: add.php');
+//        $book_id = $_POST["book_id"];
+//
+//        $sql = "SELECT * FROM books WHERE id = '$book_id'";
+//
+//        $data = mysqli_query($con, $sql);
+//
+//        $details = mysqli_fetch_row($data);
+//
+//        $title = $details[1];
+//        $author = $details[2];
+//        $publisher = $details[3];
+//        $year_of_publication = $details[4];
+//        $isbn_number = $details[5];
+//        $category = $details[6];
+//        $description = $details[7];
+//        $user_id = $details[8];
+//        $photo = $details[9];
+//        $book_id = $details[0];
 
-        $sql = "SELECT * FROM books WHERE id = '$book_id'";
-
-        $data = mysqli_query($con, $sql);
-
-        $details = mysqli_fetch_row($data);
-
-        $title = $details[1];
-        $author = $details[2];
-        $publisher = $details[3];
-        $year_of_publication = $details[4];
-        $isbn_number = $details[5];
-        $category = $details[6];
-        $description = $details[7];
-        $user_id = $details[8];
-        $photo = $details[9];
-        $book_id = $details[0];
 
     } elseif (isset($_POST['reserve_action'])) {
 
@@ -110,9 +114,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
         if ($action == 'reserve_book') {
             $reserve_book = true;
-            $book_id = $_POST["book_id"];
+            $bookId = $_POST["book_id"];
 
-            $sql = "SELECT * FROM books WHERE id = '$book_id'";
+            $sql = "SELECT * FROM books WHERE id = '$bookId'";
 
             $dataBooks = mysqli_query($con, $sql);
 
@@ -134,12 +138,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             $reserve_book = true;
 
-            $book_id = (int)$_POST['book_id'];
+            $bookId = (int)$_POST['book_id'];
             $user_id = $_SESSION['id'];
             $date = $_POST['date'];
 
-            $sqlAddReservation = "INSERT into reservations (date, user_id, book_id) VALUES ('$date', '$user_id', '$book_id')";
-
+            $sqlAddReservation = "INSERT into reservations (date, user_id, book_id) VALUES ('$date', '$user_id', '$bookId')";
+//echo $sqlAddReservation;
             $enter = mysqli_query($con, $sqlAddReservation);
 
             if ($enter) {
@@ -166,11 +170,11 @@ function getUserdetails($user_id)
 }
 
 
-function checkReserved($book_id)
+function checkReserved($bookId)
 {
     global $con;
 
-    $sql = "SELECT * FROM reservations WHERE book_id = $book_id";
+    $sql = "SELECT * FROM reservations WHERE book_id = $bookId";
 
     $res = mysqli_query($con, $sql);
 
@@ -186,6 +190,27 @@ function checkReserved($book_id)
 $sql = "select * from books";
 
 $result = mysqli_query($con, $sql);
+
+
+if(isset($_POST['reserveBook'])) {
+
+    $bookId = $_POST['book_id'];
+    //$user_id = 7;
+    $user_id = $_SESSION['id'];
+    $title = $_POST['book_title'];
+
+
+    $sqlAddReservation = "INSERT into reservations (user_id, book_id) VALUES ($user_id, $bookId)";
+
+    $enter = mysqli_query($con, $sqlAddReservation);
+
+    if ($enter) {
+        $success_message = "Book has been reserved";
+    } else {
+        $form_error = 'Process was unsuccessful';
+    }
+
+}
 
 
 ?>
@@ -218,6 +243,23 @@ $result = mysqli_query($con, $sql);
                     <a class="nav-link" href="student.php"><i style="color:pink;" class="fa fa-home"></i>Home <span class="sr-only">(current)</span></a>
                 </li>
             <?php } ?>
+            <?php if ($_SESSION["is_admin"] == 1) { ?>
+            <li class="nav-item active">
+                <a class="nav-link" href="add.php"><i style="color:pink;"class="fa fa-book-reader"></i>Add books <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="books.php"><i style="color:pink;"class="fa fa-book-open"></i>List books <span class="sr-only">(current)</span></a>
+            </li>
+            <?php } else { ?>
+                <li class="nav-item active">
+                    <a class="nav-link" href="books.php"><i style="color:pink;"class="fa fa-book-reader"></i>Books<span class="sr-only">(current)</span></a>
+                </li>
+            <?php } ?>
+            <?php if ($_SESSION["is_admin"] == 1) { ?>
+            <li class="nav-item active">
+                <a class="nav-link" href="status.php"><i style="color:pink;"class="fa fa-info"></i>View status <span class="sr-only">(current)</span></a>
+            </li>
+            <?php } ?>
             <li class="nav-item active">
                 <a class="nav-link" href="logout.php"><i style="color:pink;float: right" class="fa fa-sign-out-alt"></i>Logout<span class="sr-only">(current)</span></a>
             </li>
@@ -228,64 +270,59 @@ $result = mysqli_query($con, $sql);
 <p><?php echo $delete_error ?></p>
 <p><?php echo $success_message; ?></p>
 
-<?php if ($_SESSION["is_admin"] == 1) { ?>
-    <form action="books.php" style="width: 400px;background: #fcfcfc;margin: 70px auto;" method="POST">
-        <div class="form-group">
-            <label for="formGroupExampleInput">Title</label>
-            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Title" name="title"
-                   value="<?php echo $title; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Author</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Author" name="author"
-                   value="<?php echo $author; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Publisher</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Publisher" name="publisher"
-                   value="<?php echo $publisher; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Year of publication</label>
-            <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="year of publication"
-                   name="year_of_publication" value="<?php echo $year_of_publication; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">ISBN no.</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="ISBN no."
-                   name="isbn_number" value="<?php echo $isbn_number; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Category</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Category" name="category"
-                   value="<?php echo $category; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Title description</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Title description"
-                   name="description" value="<?php echo $description; ?>">
-        </div>
+<?php //if ($_SESSION["is_admin"] == 1) { ?>
+<!--    <form action="books.php" style="width: 400px;background: #fcfcfc;margin: 70px auto;" method="POST">-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput">Title</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Title" name="title" value="--><?php //echo $title; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">Author</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Author" name="author" value="--><?php //echo $author; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">Publisher</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Publisher" name="publisher" value="--><?php //echo $publisher; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">Year of publication</label>-->
+<!--            <input type="date" class="form-control" id="formGroupExampleInput2" placeholder="year of publication" name="year_of_publication" value="--><?php //echo $year_of_publication; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">ISBN no.</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="ISBN no." name="isbn_number" value="--><?php //echo $isbn_number; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">Category</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Category" name="category" value="--><?php //echo $category; ?><!--">-->
+<!--        </div>-->
+<!--        <div class="form-group">-->
+<!--            <label for="formGroupExampleInput2">Title description</label>-->
+<!--            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Title description" name="description" value="--><?php //echo $description; ?><!--">-->
+<!--        </div>-->
+<!---->
+<!--        <div class="form-group">-->
+<!--            <label for="exampleFormControlFile1">Example file input</label>-->
+<!--            <input type="file" class="form-control-file" id="exampleFormControlFile1" name="photo" value="--><?php //echo $photo; ?><!--">-->
+<!--        </div>-->
+<!---->
+<!---->
+<!---->
+<!--        --><?php //if (empty($book_id)) { ?>
+<!---->
+<!--            <input type="hidden" value="create_new" name="create_new">-->
+<!--        --><?php //} else { ?>
+<!--            <input type="hidden" value="create_edit" name="create_edit">-->
+<!---->
+<!--            <input type="hidden" value="--><?php //echo $book_id; ?><!--" name="book_id"/>-->
+<!--        --><?php //} ?>
+<!--        <button type="submit" class="btn btn-primary">Submit</button>-->
+<!--    </form>-->
+<?php //} ?>
 
+<?php //while ($array = mysqli_fetch_row($result)) { ?>
 
-        <div class="custom-file">
-            <input type="file" class="custom-file-input" id="customFile" name="photo" value=" <?php echo $photo; ?>">
-            <label class="custom-file-label" for="customFile">Choose file</label>
-        </div>
-
-
-        <?php if (empty($book_id)) { ?>
-
-            <input type="hidden" value="create_new" name="create_new">
-        <?php } else { ?>
-            <input type="hidden" value="create_edit" name="create_edit">
-
-            <input type="hidden" value="<?php echo $book_id; ?>" name="book_id"/>
-        <?php } ?>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-<?php } ?>
-
-<?php while ($array = mysqli_fetch_row($result)) { ?>
+<?php foreach ($result as $list) { ?>
 
 
     <div class="col-sm-4" style="margin-bottom: 30px; 4px;float: left">
@@ -294,24 +331,26 @@ $result = mysqli_query($con, $sql);
 
             <div class="card-body" style="padding-top:">
 
-                <div class="row" style="margin-bottom:20px;background-position: center;background-size:cover;background-repeat:no-repeat;background-image: URL(<?php echo $array[9]; ?>); height: 600px;">
+                <div class="row" style="margin-bottom:20px;background-position: center;background-size:cover;background-repeat:no-repeat;background-image: URL(<?php echo $list['photo']; ?>); height: 600px;">
 
                 </div>
 
-                <h5 class="card-title"><strong><?php echo $array[1]; ?></strong></h5>
+                <h5 class="card-title"><strong><?php echo $list['title']; ?></strong></h5>
 
                 <p class="card-text">
-                    <small><strong>AUTHOR:</strong><?php echo $array[2]; ?></small>
+                    <small><strong>AUTHOR: </strong><?php echo $list['author']; ?></small>
                 </p>
 
       <?php if ($_SESSION["is_admin"] == 0) { ?>
-         <?php if (checkReserved($array[0])) { ?>
+         <?php if (checkReserved($list['id'])) { ?>
               <p>
               <form action="books.php" method="POST">
-                  <input type="hidden" name="book_id" value="<?php echo $array[0]; ?>">
-                  <input type="hidden" value="reserve_book" name="reserve_action">
 
-                  <button type="submit" class="btn btn-primary">Reserve</button>
+                  <input type="hidden" name="book_id" value="<?php echo $list['id']; ?>">
+
+                  <input type="hidden" value="<?php echo $list['title']; ?>" name="book_title">
+
+                  <button type="submit" name="reserveBook" class="btn btn-primary">Reserve</button>
               </form>
               </p>
           <?php } else { ?>
@@ -322,7 +361,7 @@ $result = mysqli_query($con, $sql);
       <?php if ($_SESSION["is_admin"] == 1) { ?>
           <p>
           <form action="books.php" method="POST">
-              <input type="hidden" name="book_id" value="<?php echo $array[0]; ?>">
+              <input type="hidden" name="book_id" value="<?php echo $list['id']; ?>">
 
               <input type="hidden" value="delete_action" name="delete_action">
 
@@ -330,9 +369,9 @@ $result = mysqli_query($con, $sql);
           </form>
           </br>
 
-          <form action="books.php" method="POST">
+          <form action="add.php" method="POST">
 
-              <input type="hidden" name="book_id" value="<?php echo $array[0]; ?>">
+              <input type="hidden" name="book_id" value="<?php echo $list['id']; ?>">
 
               <input type="hidden" name="edit_action" value="edit_action">
 
@@ -354,36 +393,6 @@ $result = mysqli_query($con, $sql);
 <?php mysqli_free_result($result); ?>
 <?php mysqli_close($con); ?>
 
-<?php if ($reserve_book) { ?>
-    <form action="books.php" style="width: 400px;background: #fcfcfc;margin: 70px auto;" method="POST">
-        <div class="form-group">
-            <label for="formGroupExampleInput">Date</label>
-            <input type="date" class="form-control" id="formGroupExampleInput" placeholder="date" name="date"
-                   value="<?php echo $date; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Firstname</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="firstname" name="firstname"
-                   value="<?php echo $firstname; ?>" />
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Lastname</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="lastname" name="lastname"
-                   value="<?php echo $lastname; ?>">
-        </div>
-        <div class="form-group">
-            <label for="formGroupExampleInput2">Book title</label>
-            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="title" name="title"
-                   value="<?php echo $title; ?>">
-        </div>
-        <input type="hidden" value="reserve" name="reserve_action">
 
-        <input type="hidden" value="<?php echo $user_id; ?>" name="user_id">
-
-        <input type="hidden" value="<?php echo $book_id; ?>" name="book_id">
-
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-<?php } ?>
 </body>
 </html>
